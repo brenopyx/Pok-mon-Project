dce632e047cf5de503985e3ef0d7c5daf1b0b2c8
Breno <brenocs001@outlook.com>
2026-06-04 11:12:58 -0300

    refatoraçao

--- pokemon/test_pokemon.py
+++ pokemon/test_pokemon.py
@@ -1,38 +1,35 @@
-from pokemon import pokemon
+from pokemon import get_pokemon
 from unittest.mock import Mock
 import requests
 
 def test_pokemon_not_found(monkeypatch, capsys):
-    #Simular Input
-    monkeypatch.setattr("builtins.input", lambda _: "algo") #-> Poderia usar (builtins, "input") mas teria que importar "builtins"
 
-    #fake API
+    # Fake API
     fake_response = Mock()
     fake_response.status_code = 404
 
-    #Simular Request
+    # Simular Request
     monkeypatch.setattr(requests, "get", lambda _: fake_response) #-> Poderia usar ("request.get")
 
-    pokemon()
+    result = get_pokemon("none")
 
-    #Pegar o print
+    # Pegar o print
     captura = capsys.readouterr()
 
-    #Verifica se aparece a mensagem "Pokemon not found"
-    assert "Pokemon not found" in captura.out
+    # Verifica se aparece a mensagem "Pokemon not found"
+    assert result is None
 
 
 def test_connection_error(monkeypatch, capsys):
 
-    monkeypatch.setattr("builtins.input", lambda _: "pikachu")
 
     def mock_get(_):
         raise requests.exceptions.RequestException
     
     monkeypatch.setattr(requests, "get", mock_get)
     
-    pokemon()
+    result = get_pokemon("pikachu")
 
     captura = capsys.readouterr()
 
-    assert "The server could be not acessed" in captura.out
+    assert result is None
